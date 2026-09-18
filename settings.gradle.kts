575cb7bd08174164a750f3b5a1f40531d10e27f7
itzHiti <karim_madi@mail.ru>
2026-09-18 16:16:43 +0500

    chore: initialize LeafConfig project

---
+++ settings.gradle.kts
@@ -0,0 +1,12 @@
+rootProject.name = "LeafConfig"
+
+include(
+    "leafconfig-api",
+    "leafconfig-yaml",
+    "leafconfig-paper",
+    "leafconfig-example"
+)
+include("leafconfig-api")
+include("leafconfig-example")
+include("leafconfig-paper")
+include("leafconfig-yaml")
