7c00c25b3fb3b7366ffe6e03c66c1c867c4ed937
itzHiti <karim_madi@mail.ru>
2026-09-22 16:21:05 +0500

    chore: start 0.2.1-SNAPSHOT and point the no-shade example at 0.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ val publishedModules = setOf("leafconfig-api", "leafconfig-yaml", "leafconfig-pa
 
 allprojects {
     group = "io.github.itzhiti"
-    version = "0.2.0"
+    version = "0.2.1-SNAPSHOT"
 
     repositories {
         mavenCentral()
